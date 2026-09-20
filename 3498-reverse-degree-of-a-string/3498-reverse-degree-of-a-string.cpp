5a5c16c2da6b46ac302f6276bb616d9819004328
Neel Agrawal <neelag0804@gmail.com>
2026-09-20 22:25:02 +0530

    Time: 3 ms (16.14%), Space: 9.8 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        int i;
+        for(i=0;i<s.size();i++){
+            int rev='z'-s[i]+1;
+            ans+=rev*(i+1);
+        }return ans;
+    }
+};
